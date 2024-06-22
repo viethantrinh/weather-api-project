c690e6d2ad63a7959fbb033e5574c54abd04911f
Han Trinh Viet <hntrnn12@gmail.com>
2024-06-22 09:18:44 +0700

    initial multi-module project

---
+++ src/main/java/net/branium/WeatherApiProjectApplication.java
@@ -0,0 +1,13 @@
+package net.branium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherApiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherApiProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/branium/WeatherApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package net.branium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherApiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
